fca83ebed00dffd469d41dd0ee93ff2d843ca7ba
Lisias T <me@lisias.net>
2021-10-29 16:05:54 -0300

    Using KSPe.UI v2.4 facilities.

--- Source/AnyRes/Handler.cs
+++ Source/AnyRes/Handler.cs
@@ -28,23 +28,22 @@ using System.Text.RegularExpressions;  //Get Regex
 using UnityEngine;
 using KSP.UI.Screens;
 
-using ToolbarControl_NS;
-
+using Toolbar = KSPe.UI.Toolbar;
 using GUI = KSPe.UI.GUI;
+using GUILayout = KSPe.UI.GUILayout;
 using Data = KSPe.IO.Data<AnyRes.Startup>;
-using File = KSPe.IO.File<AnyRes.Startup>;
+using Asset = KSPe.IO.Asset<AnyRes.Startup>;
+using KSPe.Annotations;
 
 namespace AnyRes
 {
-
 	[KSPAddon(KSPAddon.Startup.AllGameScenes, false)]
 	public class AnyRes : MonoBehaviour
 	{
-
 		private static Rect anyresWinRect = new Rect(35, 99, 400, 275);
-        private Rect deleteRect = new Rect((Screen.width - 200) / 2, (Screen.height - 100) / 2, 200, 100);
+		private Rect deleteRect = new Rect((Screen.width - 200) / 2, (Screen.height - 100) / 2, 200, 100);
 
-        private string nameString = "";
+		private string nameString = "";
 		private string xString = "1280";
 		private string yString = "720";
 
@@ -55,18 +54,18 @@ namespace AnyRes
 		private bool fullScreen = true;
 		private bool reloadScene = false;
 
-        private ToolbarControl toolbarControl;
+		private Toolbar.Button toolbarButton = null;
 
-        private Util.Presets presets;
+		private Util.Presets presets;
 
-        private Vector2 scrollViewPos;
-        private bool deleteEnabled = false;
-        private bool confirmDeleteEnabled = false;
+		private Vector2 scrollViewPos;
+		private bool deleteEnabled = false;
+		private bool confirmDeleteEnabled = false;
 
 
-        void Start()
-        {
-            this.presets = gameObject.AddComponent<Util.Presets>();
+		void Start()
+		{
+			this.presets = gameObject.AddComponent<Util.Presets>();
 
 #if false
             if (HighLogic.LoadedScene == GameScenes.SETTINGS)
@@ -79,128 +78,123 @@ namespace AnyRes
             }
             else 
 #endif
-            if (HighLogic.LoadedScene == GameScenes.EDITOR)
-            {
+			if (HighLogic.LoadedScene == GameScenes.EDITOR)
+			{
 
-                anyresWinRect.x = Screen.width - 272;
-                anyresWinRect.y = Screen.height - 231;
+				anyresWinRect.x = Screen.width - 272;
+				anyresWinRect.y = Screen.height - 231;
 
-            }
+			}
 
-            Log.detail("Loaded, scene: {0}", HighLogic.LoadedScene);
+			Log.detail("Loaded, scene: {0}", HighLogic.LoadedScene);
 
-            xString = GameSettings.SCREEN_RESOLUTION_WIDTH.ToString ();
-			yString = GameSettings.SCREEN_RESOLUTION_HEIGHT.ToString ();
+			xString = GameSettings.SCREEN_RESOLUTION_WIDTH.ToString();
+			yString = GameSettings.SCREEN_RESOLUTION_HEIGHT.ToString();
 			fullScreen = GameSettings.FULLSCREEN;
 
 
-            //DontDestroyOnLoad(this);
+			//DontDestroyOnLoad(this);
 
-           if (HighLogic.LoadedScene == GameScenes.SPACECENTER ||
-                HighLogic.LoadedScene == GameScenes.EDITOR ||
-                HighLogic.LoadedScene == GameScenes.FLIGHT ||
-                HighLogic.LoadedScene == GameScenes.TRACKSTATION)
-            {
-                toolbarControl = gameObject.AddComponent<ToolbarControl>();
-                toolbarControl.AddToAllToolbars(
-                          OnTrue, OnFalse,
-                          ApplicationLauncher.AppScenes.FLIGHT | ApplicationLauncher.AppScenes.MAPVIEW |
-                          ApplicationLauncher.AppScenes.SPACECENTER | ApplicationLauncher.AppScenes.SPH |
-                          ApplicationLauncher.AppScenes.TRACKSTATION | ApplicationLauncher.AppScenes.VAB,
-                          RegisterToolbar.MODID,
-                          "AnyResButton",
-                          File.Asset.Solve("textures", "Toolbar_32"),
-                          File.Asset.Solve("textures", "Toolbar_24"),
-                          RegisterToolbar.MODNAME
-                    );
-                Log.detail("Toolbar Control registered");
-            }
+			if (HighLogic.LoadedScene == GameScenes.SPACECENTER ||
+				 HighLogic.LoadedScene == GameScenes.EDITOR ||
+				 HighLogic.LoadedScene == GameScenes.FLIGHT ||
+				 HighLogic.LoadedScene == GameScenes.TRACKSTATION)
+			{
+				this.toolbarButton = Toolbar.Button.Create(this
+						, ApplicationLauncher.AppScenes.FLIGHT | ApplicationLauncher.AppScenes.MAPVIEW
+							| ApplicationLauncher.AppScenes.SPACECENTER | ApplicationLauncher.AppScenes.SPH
+							| ApplicationLauncher.AppScenes.TRACKSTATION | ApplicationLauncher.AppScenes.VAB
+						, Asset.Texture2D.LoadFromFile("textures", "Toolbar_32")
+						, Asset.Texture2D.LoadFromFile("textures", "Toolbar_24")
+						, Version.FriendlyName
+					);
 
-        }
+				this.toolbarButton.Toolbar.Add(
+						Toolbar.Button.ToolbarEvents.Kind.Active
+						, new Toolbar.Button.Event(this.OnTrue, this.OnFalse)
+					);
 
-        void OnTrue()
-        {
-            windowEnabled = true;
-        }
-        void OnFalse()
-        {
-            windowEnabled = false;
-        }
-        public void OnDisable ()
+				ToolbarController.Instance.Add(this.toolbarButton);
+
+				Log.detail("Toolbar Control registered");
+			}
+
+		}
+
+		void OnTrue()
 		{
-            OnDestroy();
-        }
-        public void OnDestroy()
-        {
+			windowEnabled = true;
+		}
+		void OnFalse()
+		{
+			windowEnabled = false;
+		}
 
-            if (toolbarControl != null)
-            {
-                toolbarControl.OnDestroy();
-                Destroy(toolbarControl);
-            }
-        }
+		[UsedImplicitly]
+		private void OnDisable()
+		{
+			OnDestroy();
+		}
 
-        void Update() {
+		[UsedImplicitly]
+		private void OnDestroy()
+		{
+			if (toolbarButton != null)
+				ToolbarController.Instance.Destroy();
+		}
 
-            //Thanks bananashavings http://forum.kerbalspaceprogram.com/index.php?/profile/156147-bananashavings/ - https://gist.github.com/bananashavings/e698f4359e1628b5d6ef
-            //Also thanks to Crzyrndm for the fix to that code!
-            //(HighLogic.LoadedScene == GameScenes.TRACKSTATION || HighLogic.LoadedScene == GameScenes.SPACECENTER || HighLogic.LoadedScene == GameScenes.FLIGHT || HighLogic.LoadedScene == GameScenes.EDITOR)
+		[UsedImplicitly]
+		private void Update()
+		{
 
-            if ((GameSettings.MODIFIER_KEY.GetKey() ) && Input.GetKeyDown (KeyCode.Slash)) {
+			//Thanks bananashavings http://forum.kerbalspaceprogram.com/index.php?/profile/156147-bananashavings/ - https://gist.github.com/bananashavings/e698f4359e1628b5d6ef
+			//Also thanks to Crzyrndm for the fix to that code!
+			//(HighLogic.LoadedScene == GameScenes.TRACKSTATION || HighLogic.LoadedScene == GameScenes.SPACECENTER || HighLogic.LoadedScene == GameScenes.FLIGHT || HighLogic.LoadedScene == GameScenes.EDITOR)
 
+			if ((GameSettings.MODIFIER_KEY.GetKey()) && Input.GetKeyDown(KeyCode.Slash))
+			{
 				windowEnabled = !windowEnabled;
-				if (ApplicationLauncher.Ready) {
-
-					if (toolbarControl != null){
-						if (windowEnabled) {
-
-                            toolbarControl.SetTrue (true);
-
-						} else {
-
-                            toolbarControl.SetFalse (true);
-
-						}
+				if (ApplicationLauncher.Ready)
+				{
+					if (toolbarButton != null)
+					{
+						toolbarButton.Active = windowEnabled;
 					}
-
 				}
-
 			}
 
 			//Meant for debugging
 			Log.dbg("X: {0}", anyresWinRect.x);
 			Log.dbg("Y: {1}", anyresWinRect.y);
-
-
-        }
+		}
 
 		void OnGUI()
-        {
-            if (toolbarControl != null)
-            {
-                if (HighLogic.CurrentGame.Parameters.CustomParams<AR>().useKSPSkin)
-                    GUI.skin = HighLogic.Skin;
-            }
+		{
+			if (toolbarButton != null)
+			{
+				if (HighLogic.CurrentGame.Parameters.CustomParams<AR>().useKSPSkin)
+					GUI.skin = HighLogic.Skin;
+			}
 			if (windowEnabled)
-            {
+			{
 
-                if (anyresWinRect.x + anyresWinRect.width > Screen.width)
-                    anyresWinRect.x = Screen.width - anyresWinRect.width ;
-                if (anyresWinRect.y + anyresWinRect.height > Screen.height)
-                    anyresWinRect.y = Screen.height -anyresWinRect.height;
+				if (anyresWinRect.x + anyresWinRect.width > Screen.width)
+					anyresWinRect.x = Screen.width - anyresWinRect.width;
+				if (anyresWinRect.y + anyresWinRect.height > Screen.height)
+					anyresWinRect.y = Screen.height - anyresWinRect.height;
 
+				anyresWinRect.x = Math.Max(anyresWinRect.x, 0);
+				anyresWinRect.y = Math.Max(anyresWinRect.y, 0);
 
-                anyresWinRect.x = Math.Max(anyresWinRect.x, 0);
-                anyresWinRect.y = Math.Max(anyresWinRect.y, 0);
-
-                anyresWinRect = GUI.Window (09271, anyresWinRect, GUIActive, "AnyRes");
+				anyresWinRect = GUI.Window(09271, anyresWinRect, GUIActive, "AnyRes");
 
 			}
-            if (confirmDeleteEnabled)
-                deleteRect = GUI.Window(09276, deleteRect, ConfirmDelete, "Confirm");
-        }
+			if (confirmDeleteEnabled)
+				deleteRect = GUI.Window(09276, deleteRect, ConfirmDelete, "Confirm");
+		}
 
-		void GUIActive(int windowID) {
+		void GUIActive(int windowID)
+		{
 #if false
             if (HighLogic.LoadedScene == GameScenes.SETTINGS) {
 
@@ -208,178 +202,184 @@ namespace AnyRes
 
 			}
 #endif
-            GUILayout.BeginHorizontal();
+			GUILayout.BeginHorizontal();
 
-            GUILayout.BeginVertical ();
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Name: ");
-            nameString = GUILayout.TextField(nameString);
-            GUILayout.EndHorizontal();
+			GUILayout.BeginVertical();
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Name: ");
+			nameString = GUILayout.TextField(nameString);
+			GUILayout.EndHorizontal();
 
-            GUILayout.BeginHorizontal ();
-			GUILayout.Label ("Width: ");
-			xString = GUILayout.TextField (xString);
-			xString = Regex.Replace (xString, @"[^0-9]", "");
-			GUILayout.EndHorizontal ();
-			GUILayout.BeginHorizontal ();
-			GUILayout.Label ("Height: ");
-			yString = GUILayout.TextField (yString);
-			yString = Regex.Replace (yString, @"[^0-9]", "");
-			GUILayout.EndHorizontal ();
-			fullScreen = GUILayout.Toggle (fullScreen, "Fullscreen");
-			reloadScene = GUILayout.Toggle (reloadScene, "Reload scene");
-			if (GUILayout.Button("Set Screen Resolution")) {
-
-				if (xString != null && yString != null) {
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Width: ");
+			xString = GUILayout.TextField(xString);
+			xString = Regex.Replace(xString, @"[^0-9]", "");
+			GUILayout.EndHorizontal();
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Height: ");
+			yString = GUILayout.TextField(yString);
+			yString = Regex.Replace(yString, @"[^0-9]", "");
+			GUILayout.EndHorizontal();
+			fullScreen = GUILayout.Toggle(fullScreen, "Fullscreen");
+			reloadScene = GUILayout.Toggle(reloadScene, "Reload scene");
+			if (GUILayout.Button("Set Screen Resolution"))
+			{
+				if (xString != null && yString != null)
+				{
 
 					x = Convert.ToInt32(xString);
 					y = Convert.ToInt32(yString);
 
-					if (x > 0 && y > 0) {
+					if (x > 0 && y > 0)
+					{
 
 						GameSettings.SCREEN_RESOLUTION_HEIGHT = y;
 						GameSettings.SCREEN_RESOLUTION_WIDTH = x;
 						GameSettings.FULLSCREEN = fullScreen;
-						GameSettings.SaveSettings ();
+						GameSettings.SaveSettings();
 						Screen.SetResolution(x, y, fullScreen);
 						Log.detail("Set screen resolution");
 
-						if (reloadScene) {
-							if (HighLogic.LoadedScene != GameScenes.LOADING) {
+						if (reloadScene)
+						{
+							if (HighLogic.LoadedScene != GameScenes.LOADING)
+							{
 								HighLogic.LoadScene(HighLogic.LoadedScene);
-							} else {
+							}
+							else
+							{
 								ScreenMessages.PostScreenMessage("You cannot reload the scene while loading the game!", 1);
 							}
 						}
-					} else {
+					}
+					else
+					{
 						ScreenMessages.PostScreenMessage("One or both of your values is too small.  Please enter a valid value.", 1, ScreenMessageStyle.UPPER_CENTER);
 					}
-				} else {
+				}
+				else
+				{
 					ScreenMessages.PostScreenMessage("The values you have set are invalid.  Please set a valid value.", 1, ScreenMessageStyle.UPPER_CENTER);
 				}
-
 			}
 
-            if (nameString == "")
-                GUI.enabled = false;
+			if (nameString == "")
+				GUI.enabled = false;
 
-            if (GUILayout.Button("Save"))
-            {
+			if (GUILayout.Button("Save"))
+			{
 				string newName = nameString;
 				string newX = xString;
 				string newY = yString;
 				bool newFullscreen = fullScreen;
 
-                ConfigNode config = new ConfigNode(newName);
-                config.AddValue("name", newName);
-                config.AddValue("x", newX);
-                config.AddValue("y", newY);
-                config.AddValue("fullscreen", newFullscreen.ToString());
-                this.presets.Create(config);
+				ConfigNode config = new ConfigNode(newName);
+				config.AddValue("name", newName);
+				config.AddValue("x", newX);
+				config.AddValue("y", newY);
+				config.AddValue("fullscreen", newFullscreen.ToString());
+				this.presets.Create(config);
 
-                ScreenMessages.PostScreenMessage("Preset saved.  You can change the preset later by using the same name in this editor.", 5, ScreenMessageStyle.UPPER_CENTER);
-                this.presets.ReloadFiles();
-            }
+				ScreenMessages.PostScreenMessage("Preset saved.  You can change the preset later by using the same name in this editor.", 5, ScreenMessageStyle.UPPER_CENTER);
+				this.presets.ReloadFiles();
+			}
 
 
-            if (0 == this.presets.files.Count)
-                GUI.enabled = false;
-            else
-                GUI.enabled = true;
+			GUI.enabled = 0 != this.presets.files.Count;
 
-            if (deleteEnabled)
-            {
-                if (GUILayout.Button("Disable Delete"))
-                {
-                    deleteEnabled = false;
-                }
-            }
-            else
-            {
-                if (GUILayout.Button("Enable Delete"))
-                {
-                    deleteEnabled = true;
-                }
-            }
-            if (GUILayout.Button("Close"))
-            {
-                toolbarControl.SetFalse(true);
-            }
-            GUILayout.EndVertical();
-            GUILayout.BeginVertical();
-         
-            scrollViewPos = GUILayout.BeginScrollView(scrollViewPos);
+			if (deleteEnabled)
+			{
+				if (GUILayout.Button("Disable Delete"))
+				{
+					deleteEnabled = false;
+				}
+			}
+			else
+			{
+				if (GUILayout.Button("Enable Delete"))
+				{
+					deleteEnabled = true;
+				}
+			}
+			if (GUILayout.Button("Close"))
+			{
+				toolbarButton.Active = false;
+			}
+			GUILayout.EndVertical();
+			GUILayout.BeginVertical();
+
+			scrollViewPos = GUILayout.BeginScrollView(scrollViewPos);
 
 			foreach (Data.ConfigNode configNode in this.presets.files)
 			{
 				ConfigNode config = configNode.Node;
-                if (deleteEnabled)
-                {
-                    if (GUILayout.Button("Delete " + config.GetValue("name")))
-                    {
-                        confirmDeleteEnabled = true;
-                        this.presets.MarkForDeletion(configNode);
-                    }
-                }
-                else
-                {
-                    if (GUILayout.Button(config.GetValue("name")))
-                    {
-                        int xVal;
-                        int.TryParse(config.GetValue("x"), out xVal);
-                        int yVal;
-                        int.TryParse(config.GetValue("y"), out yVal);
-                        bool fullscreen;
-                        bool.TryParse(config.GetValue("fullscreen"), out fullscreen);
-                        GameSettings.SCREEN_RESOLUTION_HEIGHT = yVal;
-                        GameSettings.SCREEN_RESOLUTION_WIDTH = xVal;
-                        GameSettings.FULLSCREEN = fullscreen;
-                        GameSettings.SaveSettings();
-                        Screen.SetResolution(xVal, yVal, fullscreen);
-                        Log.detail("Set screen resolution from preset");
-                    }
-                }
-            }
+				if (deleteEnabled)
+				{
+					if (GUILayout.Button("Delete " + config.GetValue("name")))
+					{
+						confirmDeleteEnabled = true;
+						this.presets.MarkForDeletion(configNode);
+					}
+				}
+				else
+				{
+					if (GUILayout.Button(config.GetValue("name")))
+					{
+						int xVal;
+						int.TryParse(config.GetValue("x"), out xVal);
+						int yVal;
+						int.TryParse(config.GetValue("y"), out yVal);
+						bool fullscreen;
+						bool.TryParse(config.GetValue("fullscreen"), out fullscreen);
+						GameSettings.SCREEN_RESOLUTION_HEIGHT = yVal;
+						GameSettings.SCREEN_RESOLUTION_WIDTH = xVal;
+						GameSettings.FULLSCREEN = fullscreen;
+						GameSettings.SaveSettings();
+						Screen.SetResolution(xVal, yVal, fullscreen);
+						Log.detail("Set screen resolution from preset");
+					}
+				}
+			}
 
-            GUILayout.EndScrollView();
-            GUILayout.EndVertical();
-            GUILayout.EndHorizontal();
+			GUILayout.EndScrollView();
+			GUILayout.EndVertical();
+			GUILayout.EndHorizontal();
 
-            if (GUI.Button(new Rect(anyresWinRect.width - 18, 3f, 15f, 15f), new GUIContent("X")))
-            {
-                toolbarControl.SetFalse(true);
-            }
+			if (GUI.Button(new Rect(anyresWinRect.width - 18, 3f, 15f, 15f), new GUIContent("X")))
+			{
+				toolbarButton.Active = false;
+			}
 
-            GUI.DragWindow ();
+			GUI.DragWindow();
 
 		}
 
-        void ConfirmDelete(int id)
-        {
-            GUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
-            GUILayout.Label("Confirm delete of " + this.presets.GetVictimName());
-            GUILayout.FlexibleSpace();
-            GUILayout.EndHorizontal();
-            GUILayout.FlexibleSpace();
-            GUILayout.BeginHorizontal();
-            if (GUILayout.Button("Cancel"))
-            {
-                deleteEnabled = false;
-                confirmDeleteEnabled = false;
-            }
+		void ConfirmDelete(int id)
+		{
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			GUILayout.Label("Confirm delete of " + this.presets.GetVictimName());
+			GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+			GUILayout.FlexibleSpace();
+			GUILayout.BeginHorizontal();
+			if (GUILayout.Button("Cancel"))
+			{
+				deleteEnabled = false;
+				confirmDeleteEnabled = false;
+			}
 
-            if (GUILayout.Button("Yes"))
-            {
-                //deleteEnabled = false;
-                confirmDeleteEnabled = false;
-                this.presets.Commit();
-            }
+			if (GUILayout.Button("Yes"))
+			{
+				//deleteEnabled = false;
+				confirmDeleteEnabled = false;
+				this.presets.Commit();
+			}
 
-            GUILayout.EndHorizontal();
-        }
+			GUILayout.EndHorizontal();
+		}
 
-    }
-   
+	}
+
 }
 
---
+++ Source/AnyRes/ToolbarSupport.cs
@@ -0,0 +1,39 @@
+﻿/*
+	This file is part of AnyRes /L Unleashed
+		© 2021 Lisias T : http://lisias.net <support@lisias.net>
+		© 2017-2020 LinuxGuruGamer
+		© 2015-2016 CriftonM (Crifton Marien)
+
+	AnyRes /L Unleashed is licensed as follows:
+
+		* CC BY-NC-SA 4.0i : https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+	Connected Living Space /L Unleashed is distributed in the hope that
+	it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+	warranty of	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+*/
+using System.Collections.Generic;
+
+using UnityEngine;
+using KSP.UI.Screens;
+
+using KSPe.Annotations;
+using Toolbar = KSPe.UI.Toolbar;
+using GUI = KSPe.UI.GUI;
+using GUILayout = KSPe.UI.GUILayout;
+
+namespace AnyRes
+{
+	[KSPAddon(KSPAddon.Startup.MainMenu, true)]
+	public class ToolbarController : MonoBehaviour
+	{
+		internal static KSPe.UI.Toolbar.Toolbar Instance => KSPe.UI.Toolbar.Controller.Instance.Get<ToolbarController>();
+
+		[UsedImplicitly]
+		private void Start()
+		{
+			KSPe.UI.Toolbar.Controller.Instance.Register<ToolbarController>(Version.FriendlyName);
+		}
+	}
+}
